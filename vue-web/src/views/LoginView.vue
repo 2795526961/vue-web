957f7483c3ef58b64620c5c7088eb435f7e35672
liangjingbo <2795526961@qq.com>
2023-03-19 00:43:28 +0800

    登录

--- vue-web/src/views/LoginView.vue
+++ vue-web/src/views/LoginView.vue
@@ -1,35 +1,36 @@
 <template>
-    <el-form ref="ruleFormRef" :model="ruleForm" status-icon :rules="rules" label-width="120px" class="demo-ruleForm">
-        
+    <el-form ref="ruleFormRef" :model="logindata" status-icon :rules="rules" label-width="120px"
+        class="demo-ruleForm">
+
         <el-form-item label="账号：" prop="userName">
-            <el-input v-model="ruleForm.userName" type="userName" autocomplete="off" />
+            <el-input v-model="logindata.userName" type="userName" autocomplete="off" />
         </el-form-item>
 
         <el-form-item label="密码：" prop="passWord">
-            <el-input v-model="ruleForm.passWord" type="passWord" autocomplete="off" />
+            <el-input v-model="logindata.passWord" type="passWord" autocomplete="off" />
         </el-form-item>
 
         <el-form-item>
-      <el-button class="loginbtn" type="primary" @click="submitbtn"
-        >登录</el-button
-      >
-      <el-button class="resetbtn" @click="resetbtn">重置</el-button>
-    </el-form-item>
+            <el-button class="loginbtn" type="primary" @click="submitbtn">登录</el-button>
+        </el-form-item>
 
     </el-form>
 </template>
 
 <script lang="ts">
 import { defineComponent, reactive, toRefs } from 'vue'
-
+import { ElMessage } from 'element-plus'
+import $http from '../request/index'
+import  router from '../router/index'
 export default defineComponent({
     setup() {
-        const data = reactive({
-            ruleForm: {
+        //登录的model提交数据
+        const logindata = reactive({
                 userName: "",
                 passWord: ""
-            },
-            rules: {
+        })
+        //规则
+        const rules=reactive({
                 userName: [
                     { required: true, message: '不能空', trigger: 'blur' },
                     { min: 3, max: 5, message: '长度必须3 to 5', trigger: 'blur' },
@@ -41,29 +42,51 @@ export default defineComponent({
                         trigger: 'change',
                     },
                 ],
+        })
+        //登录方法
+        let submitbtn = () => {
+            if (!logindata.userName || !logindata.passWord) {
+                ElMessage.error('空')
+                return
             }
 
-        })
+        //    let Geturl='/GetUser?'+'username='+logindata.ruleForm.userName
+        //     $http.get(Geturl).then(res=>{
+        //         console.log(res);
+                
+        //     })
+
+            let PostUrl='/PostUser'
+            var temp={
+                "username":logindata.userName,
+                "password":logindata.passWord
+            }
+
+            $http.post(PostUrl,temp).then(res=>{
+
+                console.log(res);
+                //跳转
+                router.push('/test')
+            })
+
+        }
 
 
-        return { ...toRefs(data) }
+
+
+
+        return { logindata, submitbtn ,rules}
     }
-
-    
 })
 
-let submitbtn=()=>{
-    console.log(123);
-    
-}
+
 
 </script>
 
 <style scoped>
-.demo-ruleForm{
-width: 500px;
-margin: 200px auto;
+.demo-ruleForm {
+    width: 500px;
+    margin: 200px auto;
 
 }
-
 </style>
---
+++ vue-web/src/views/TestView.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+            123321
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+import $http from '../request/index'
+
+export default defineComponent({
+    setup () {
+        $http.get('/GetUserList')
+        return {}
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
